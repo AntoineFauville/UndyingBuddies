79086fcd29c9b99d20bfdc2672a921402a531e8d
sheido <af8081@gmail.com>
2020-01-11 12:38:33 +0100

    Menu

---
+++ UndyingBuddies/Assets/Scripts/SceneLoadLocal.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneLoadLocal : MonoBehaviour
+{
+
+    SceneManagerDontDestroy SceneManagerDontDestroy;
+
+    [SerializeField] private Animator animator;
+
+    void Start()
+    {
+        animator.enabled = false;
+        SceneManagerDontDestroy = GameObject.Find("SceneManager").GetComponent<SceneManagerDontDestroy>();
+    }
+
+    public void Play()
+    {
+        StartCoroutine(waitforSec());
+    }
+
+    IEnumerator waitforSec()
+    {
+        animator.enabled = true;
+        animator.Play("launchGame");
+
+        yield return new WaitForSeconds(0.45f);
+        SceneManagerDontDestroy.LoadScene();
+    }
+}
