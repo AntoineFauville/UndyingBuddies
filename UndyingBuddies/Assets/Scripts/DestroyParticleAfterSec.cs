35f103d03d1a2cef1fc302d5fa245de2d80da594
sheido <af8081@gmail.com>
2020-01-12 15:29:47 +0100

    aura de feu revisite

---
+++ UndyingBuddies/Assets/Scripts/DestroyParticleAfterSec.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyParticleAfterSec : MonoBehaviour
+{
+    [SerializeField] private float seconds;
+
+    [SerializeField] private ParticleSystem[] particleSystems;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(waittoDie());
+    }
+
+    IEnumerator waittoDie()
+    {
+        yield return new WaitForSeconds(seconds);
+
+        for (int i = 0; i < particleSystems.Length; i++)
+        {
+            particleSystems[i].enableEmission = false;
+        }
+
+        yield return new WaitForSeconds(2);
+        DestroyImmediate(this.gameObject);
+    }
+}
