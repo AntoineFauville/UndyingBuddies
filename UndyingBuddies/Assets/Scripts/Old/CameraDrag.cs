2d121008995fec8a0c19562974ed38622bb7f0d7
sheido <af8081@gmail.com>
2020-01-21 10:14:36 +0100

    preparation for new concept

--- UndyingBuddies/Assets/Scripts/Old/CameraDrag.cs
+++ UndyingBuddies/Assets/Scripts/Old/CameraDrag.cs
@@ -5,9 +5,15 @@ public class CameraDrag : MonoBehaviour
     public float dragSpeed = 2;
     private Vector3 dragOrigin;
 
+    public float moveSpeed = 0.1f;
 
     void Update()
     {
+        Vector3 currentCamPos = Camera.main.transform.position;
+        Vector3 keymove = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, 0, Input.GetAxis("Vertical") * moveSpeed);
+
+        transform.Translate(keymove, Space.World);
+
         if (Input.GetMouseButtonDown(1))
         {
             dragOrigin = Input.mousePosition;
@@ -15,11 +21,13 @@ public class CameraDrag : MonoBehaviour
         }
 
         if (!Input.GetMouseButton(1)) return;
-
+        
         Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
-        Vector3 move = new Vector3(pos.x * dragSpeed, 0, pos.y * dragSpeed);
+        Vector3 move = new Vector3(pos.x * dragSpeed,0, pos.y * dragSpeed);
 
         transform.Translate(-move, Space.World);
+
+       
     }
 
 
