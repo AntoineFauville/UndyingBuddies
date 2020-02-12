d400a19a564c39fb4504a17ac0fef10b9f93d9a7
sheido <af8081@gmail.com>
2020-02-12 23:20:42 +0100

    new build. cost of spell and damage it does is missing and also when you loose or win you can't replay the game again.

--- UndyingBuddies/Assets/Scripts/Tutorial.cs
+++ UndyingBuddies/Assets/Scripts/Tutorial.cs
@@ -26,32 +26,32 @@ public class Tutorial : MonoBehaviour
     void Start()
     {
         //part 1 //setup
-        title.text = "You just got summoned!";
+        title.text = "1/6 You just got summoned!";
         tutorialContent.text = "Two scouts demons have spotted villages of priest near them, scrared, they created a small autel to summon you." + "\n" + "\n" +
             "Your goal is to destroy the priests inside those villages, scouts can't fight, you'll have to take care of that yourself!";
 
         //part 2 // key to success and how to get the goal
-        title2.text = "Energy is key";
+        title2.text = "2/6 Energy is key";
         tutorialContent2.text = "To kill the priest use spells, they can be found in the spell panel, you will need energy to unlock and use spells" + "\n" + "\n" +
             "This energy can be obtained by sacrificing resources";
 
         //part 3 //core mechanic
-        title3.text = "Sacrifice";
+        title3.text = "3/6 Sacrifice";
         tutorialContent3.text = "To sacrifice left clic on an object you want to sacrifice, if it can be sacrificed, it'll turn into resources and energy" + "\n" + "\n" +
             "At first sacrifice tree and bushes to gain energy, this is not a very efficient way to get energy, you should process the resource to get more out of these basic objects";
 
         //part 3 //processing
-        title4.text = "Processing";
+        title4.text = "4/6 Processing";
         tutorialContent4.text = "First Create a processing building, once placed, it's instantly built and if you have an available demon, he'll start to work" + "\n" + "\n" +
             "He'll transform the trees, into planks and bushes into food baskets, these refined goods once in the stockpile, can be sacrificed for a good chunk of energy";
 
         //part 5 //attacking
-        title5.text = "Attacking";
+        title5.text = "5/6 Attacking";
         tutorialContent5.text = "Be ready when you attack a city, all the priest will run to your village" + "\n" + "\n" +
             "Kill all of them before they reach you, if your cityhall gets destroyed, you loose";
 
         //part 6 //tips attacking
-        title6.text = "Tips on Attacking";
+        title6.text = "6/6 Tips on Attacking";
         tutorialContent6.text = "In order to best choose your spells, inspect the cities, these have resistance to either physical damage or sanity damage, it's different every game" + "\n" + "\n" +
             "Since the spell unlock cost increases, choose carefully wich spells to purchase to addapt the best to the situation";
 
