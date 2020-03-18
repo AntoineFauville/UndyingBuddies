3e6861749d4f00a0a2dd10af041b1e6180823f94
sheido <af8081@gmail.com>
2020-03-18 18:53:00 +0100

    rats animation and explosion poison

---
+++ UndyingBuddies/Assets/Scripts/Rats.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Rats : MonoBehaviour
+{
+    [SerializeField] private NavMeshAgent _navMeshAgent;
+
+    [SerializeField] private GameObject Target;
+
+    int distance = 2;
+
+    void Update()
+    {
+        Vector3 pos = Target.transform.position;
+        _navMeshAgent.destination = new Vector3(Random.Range(pos.x- distance, pos.x+ distance), 0, Random.Range(pos.z - distance, pos.z + distance));
+    }
+}
